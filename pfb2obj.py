e61ea629382516a986e80327b50bae6fad84a46b
Dave Pape <depape@buffalo.edu>
2025-05-25 11:05:35 -0400

    add geostate & material, plus other changes

--- pfb2obj.py
+++ pfb2obj.py
@@ -3,6 +3,17 @@ from pfb_constants import *
 
 ENDIAN_FLAG = '>'
 
+# just guessing the value of PF_MAX_TEXTURES_19 for now - need to find that and add it to pfb_constants
+# guess is almost definitely wrong, but the models I'm working with don't use this feature
+PF_MAX_TEXTURES_19 = 19
+
+# making a logical assumption for the values of PF_OFF & PF_ON - doublecheck this, then move this stuff to pfb_constants
+PF_OFF = 0
+PF_ON = 1
+OnOff_table = [ 2, PF_OFF, PF_ON ]
+
+
+
 def readInt32(f):
     data = f.read(4)
     if data == "":
@@ -120,7 +131,8 @@ def readTex(version,f):
         f.read(SIZEOF_CLIPTEX_T)
         if t.num_levels > 0:
             f.read(t.num_levels * SIZEOF_CLIPLEVEL_T)
-    return {}
+    t.filename = name
+    return t
 
 
 def isGroupClassType(t):
@@ -141,12 +153,12 @@ def readNode(version,f):
         if node.type == N_GROUP:
             node.children = [buf.pop(0) for i in range(count)]
         else:
-            print('#currently unsupported group node type')
+            print(f'#currently unsupported group node type "{node.type}"')
     elif node.type == N_GEODE:
         count = buf.pop(0)
         node.gsets = [buf.pop(0) for i in range(count)]
     else:
-        print('#currently unsupported node type')
+        print(f'#currently unsupported node type "{node.type}"')
     node.isect_travmask = buf.pop(0)
     node.app_travmask = buf.pop(0)
     node.cull_travmask = buf.pop(0)
@@ -156,6 +168,18 @@ def readNode(version,f):
         node.name = f.read(name_size).decode('ascii')
     return node
 
+
+class Mtl_data:
+    def __init__(self,version,f):
+        self.side = readInt32(f)
+        self.alpha = readFloat32(f)
+        self.shininess = readFloat32(f)
+        self.ambient = readPfVec3(f)
+        self.diffuse = readPfVec3(f)
+        self.specular = readPfVec3(f)
+        self.emission = readPfVec3(f)
+        self.cmode = readInt32Array(f,2)
+        self.udata = readInt32(f)
 
 class Gset_data:
     def __init__(self,version,f):
@@ -184,13 +208,58 @@ class Gset_data:
         if version >= PFBV_GSET_BBOX_FLUX:
             self.bbox_flux = readInt32(f)
         if version >= PFBV_MULTITEXTURE:
-            self.multi_tlist = readInt32Array(f,3*(19-1)) # guessing that PF_MAX_TEXTURES_19 is 19 - need to find that
-
+            self.multi_tlist = readInt32Array(f,3*(PF_MAX_TEXTURES_19-1))
 
 def readGset(version,f):
     gset = Gset_data(version,f)
     return gset
 
+
+def readMtl(version,f):
+    mtl = Mtl_data(version,f)
+    return mtl
+
+
+class Gstate_data:
+    def __init__(self):
+        self.enlighting = False
+        self.entexture = False
+        self.frontmtl = -1
+        self.texture = -1
+    def read(self,version,f):
+        buf_size = readInt32(f)
+#NOTE: not all of the data in buf is actually Int32's - some of it can be 32-bit floats (for alpharef and for texture matrices); the original PFB code would use pointer casting to convert those parts to floats; may be able to do the same thing with Python's struct functions, but it could be messy
+        buf = readInt32Array(f,buf_size)
+        pos = 0
+        endpos = buf_size - 1
+        while pos < endpos:
+            statemode = buf[pos]
+            pos += 1
+            if statemode == STATE_ENLIGHTING:
+                self.enlighting = (OnOff_table[buf[pos]] == PF_ON)
+                pos += 1
+            elif statemode == STATE_ENTEXTURE:
+                if version >= PFBV_MULTITEXTURE:
+                    # would need to replace add a multitex alternative to "entexture"
+                    pos += PF_MAX_TEXTURES_19-1
+                else:
+                    self.entexture = (OnOff_table[buf[pos]] == PF_ON)
+                    pos += 1
+            elif statemode == STATE_FRONTMTL:
+                #NOTE: this is an index into the list 'data.mtl'; in original PFB code it assumed the mtl info came first, so it was looked up at this point to set the material number with pfGStateAttr
+                self.frontmtl = buf[pos]
+                pos += 1
+            elif statemode == STATE_TEXTURE:
+                #NOTE: this is an index into the list 'data.tex'
+                self.texture = buf[pos]
+                pos += 1
+            else:
+                print('#WARNING: unsupported geostate attribute; this geostate may be messed up as a result')
+
+def readGstate(version,f):
+    gstate = Gstate_data()
+    gstate.read(version,f)
+    return gstate
 
 
 def readLlist(version,f):
@@ -233,7 +302,9 @@ def readIlist(version,f):
 class modelData:
     def __init__(self):
         self.node = []
+        self.mtl = []
         self.gset = []
+        self.gstate = []
         self.llist = []
         self.vlist = []
         self.clist = []
@@ -270,9 +341,15 @@ while True:
         elif listtype == L_NODE:
             for i in range(numobjects):
                 data.node.append(readNode(version,f))
+        elif listtype == L_MTL:
+            for i in range(numobjects):
+                data.mtl.append(readMtl(version,f))
         elif listtype == L_GSET:
             for i in range(numobjects):
                 data.gset.append(readGset(version,f))
+        elif listtype == L_GSTATE:
+            for i in range(numobjects):
+                data.gstate.append(readGstate(version,f))
         elif listtype == L_LLIST:
             for i in range(numobjects):
                 data.llist.append(readLlist(version,f))
@@ -292,51 +369,115 @@ while True:
             for i in range(numobjects):
                 data.ilist.append(readIlist(version,f))
         else:
-            print('#  currently unsupported - skipping')
+            print(f'#  currently unsupported list type "{listtype}"- skipping')
             f.read(numbytes)
     except Exception as error:
         break
 
 f.close()
 
+
+
+
+obj_filename = sys.argv[2]
+if obj_filename[-4:] == '.obj':
+    mtl_filename = obj_filename[:-4] + '.mtl'
+else:
+    mtl_filename = obj_filename + '.mtl'
+
+mtl_file = open(mtl_filename,'w')
+
+i=0
+for gstate in data.gstate:
+    mtl_file.write(f'newmtl gstate{i}\n')
+    if gstate.frontmtl > -1:
+        mtl = data.mtl[gstate.frontmtl]
+        mtl_file.write(f'Ka {mtl.ambient[0]} {mtl.ambient[1]} {mtl.ambient[2]}\n')
+        mtl_file.write(f'Kd {mtl.diffuse[0]} {mtl.diffuse[1]} {mtl.diffuse[2]}\n')
+        mtl_file.write(f'Ks {mtl.specular[0]} {mtl.specular[1]} {mtl.specular[2]}\n')
+        mtl_file.write(f'Ns {mtl.shininess}\n')
+    if gstate.texture > -1:
+        tex = data.tex[gstate.texture]
+        mtl_file.write(f'Kd_map {tex.filename}\n')
+    mtl_file.write('\n')
+    i += 1
+
+mtl_file.close()
+
+obj_file = open(obj_filename,'w')
+obj_file.write(f'mtllib {mtl_filename}\n')
+
+def print_tristrip_triangle(firstindex, normal_mode, texc_mode, ccw):
+    if ccw:
+        if texc_mode == PFGS_OFF:
+            obj_file.write(f'f {firstindex+1}//{firstindex+1} {firstindex+2}//{firstindex+2} {firstindex+3}//{firstindex+3}\n')
+        else:
+            obj_file.write(f'f {firstindex+1}/{firstindex+1}/{firstindex+1} {firstindex+2}/{firstindex+2}/{firstindex+2} {firstindex+3}/{firstindex+3}/{firstindex+3}\n')
+    else:
+        if texc_mode == PFGS_OFF:
+            obj_file.write(f'f {firstindex+1}//{firstindex+1} {firstindex+3}//{firstindex+3} {firstindex+2}//{firstindex+2}\n')
+        else:
+            obj_file.write(f'f {firstindex+1}/{firstindex+1}/{firstindex+1} {firstindex+3}/{firstindex+3}/{firstindex+3} {firstindex+2}/{firstindex+2}/{firstindex+2}\n')
 numverts = 0
 
 for n in data.node:
     if n.type == N_GEODE:
         for g in n.gsets:
+            coords = []
+            norms = []
+            texc = []
             gset = data.gset[g]
-            print(f'#gset type {gset.ptype}   numprims {gset.pcount}')
+            obj_file.write(f'#gset type {gset.ptype}   numprims {gset.pcount}   gstate {gset.gstate}\n')
             if gset.llist != -1:
                 lens = data.llist[gset.llist]
-                print(f'#  lenlist: {lens}')
+                obj_file.write(f'#  lenlist: {lens}\n')
             if gset.vlist[1] != -1:
                 coords = data.vlist[gset.vlist[1]]
             else:
-                continue
+                continue    # i.e. if there's no list of vertices, just skip this geoset
             if gset.vlist[2] != -1:
                 indices = data.ilist[gset.vlist[2]]
                 coords = [coords[i] for i in indices]
             vstart = numverts
             for v in coords:
-                print(f'v {v[0]} {v[1]} {v[2]}')
+                obj_file.write(f'v {v[0]} {v[1]} {v[2]}\n')
                 numverts += 1
+#NOTE: this code assumes that normals & texcoords are per-vertex.  Support for PFGS_OVERALL and PFGS_PER_PRIM will need to be added.
+            normal_mode = gsb_table[gset.nlist[0]]
+            if normal_mode != PFGS_PER_VERTEX:
+                obj_file.write('#WARNING: unsupported normal mode !!!!\n')
             if gset.nlist[1] != -1:
                 norms = data.nlist[gset.nlist[1]]
                 if gset.nlist[2] != -1:
                     indices = data.ilist[gset.nlist[2]]
                     norms = [norms[i] for i in indices]
                 for n in norms:
-                    print(f'vn {n[0]} {n[1]} {n[2]}')
+                    obj_file.write(f'vn {n[0]} {n[1]} {n[2]}\n')
+            texc_mode = gsb_table[gset.tlist[0]]
+            if (texc_mode != PFGS_PER_VERTEX) and (texc_mode != PFGS_OFF):
+                obj_file.write('#WARNING: unsupported texture coordinate mode !!!!\n')
+            if gset.tlist[1] != -1:
+                texc = data.tlist[gset.tlist[1]]
+                if gset.tlist[2] != -1:
+                    indices = data.ilist[gset.tlist[2]]
+                    texc = [texc[i] for i in indices]
+                for t in texc:
+                    obj_file.write(f'vt {t[0]} {t[1]}\n')
+            if (len(coords) != len(norms)) or (len(coords) != len(texc)):
+                obj_file.write('#ERROR: mismatched number of normals or texture coordinates - only per-vertex is supported !!!!\n')
+#NOTE: should look up how pfGSetGStateIndex() works (if gstate[1] is not -1); probably none of our models use that feature, however
+            if gset.gstate[0] != -1:
+                obj_file.write(f'usemtl gstate{gset.gstate[0]}\n')
             if gset.ptype == PFGS_TRISTRIPS:
                 stripstart = vstart
                 for striplen in lens:
                     vertindex = stripstart
                     ccw = True
                     for tri in range(striplen-2):
-                        if ccw:
-                            print(f'f {vertindex+1}//{vertindex+1} {vertindex+2}//{vertindex+2} {vertindex+3}//{vertindex+3}')
-                        else:
-                            print(f'f {vertindex+1}//{vertindex+1} {vertindex+3}//{vertindex+3} {vertindex+2}//{vertindex+2}')
+                        print_tristrip_triangle(vertindex, normal_mode, texc_mode, ccw)
                         vertindex += 1
                         ccw = not ccw
                     stripstart += striplen
+
+obj_file.close()
+
